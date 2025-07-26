133af2dee191649d4c576310a2f9865c572b417f
Moazzam Shaikh <moazzamshaikh8982@gmail.com>
2025-07-26 16:36:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+# Force Python version from runtime.txt
+echo "Forcing correct Python version from runtime.txt"
+
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
